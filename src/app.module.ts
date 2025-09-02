a0e724bd729844725f69a6c7cf8f2515d9b38909
imran-binhasan <imranbinhasan.work@gmail.com>
2025-09-02 10:17:20 +0600

    init with nestjs/graphql

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { AppService } from './app.service';
+import { AppResolver } from './app.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true, // Enable GraphQL Playground in dev
+    }),
+  ],
+  providers: [AppResolver, AppService],
+})
+export class AppModule {}
