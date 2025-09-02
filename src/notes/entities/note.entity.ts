1ee33686ac7dda69ab278aff90f9497271a5cd77
imran-binhasan <imranbinhasan.work@gmail.com>
2025-09-03 05:40:36 +0600

    add note entity

---
+++ src/notes/entities/note.entity.ts
@@ -0,0 +1,75 @@
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { User } from '../../users/entities/user.entity';
+import { Contact } from '../../contacts/entities/contact.entity';
+import { Company } from '../../companies/entities/company.entity';
+import { Lead } from '../../leads/entities/lead.entity';
+import { Deal } from '../../deals/entities/deal.entity';
+import { Activity } from '../../activities/entities/activity.entity';
+
+@ObjectType()
+export class Note {
+  @Field(() => ID)
+  id: string;
+
+  @Field(() => String)
+  title: string;
+
+  @Field(() => String)
+  content: string;
+
+  @Field(() => String, { nullable: true })
+  category?: string;
+
+  @Field(() => Boolean, { defaultValue: false })
+  isPrivate: boolean;
+
+  @Field(() => [String], { defaultValue: [] })
+  tags: string[];
+
+  // Related entities
+  @Field(() => String, { nullable: true })
+  contactId?: string;
+
+  @Field(() => Contact, { nullable: true })
+  contact?: Contact;
+
+  @Field(() => String, { nullable: true })
+  companyId?: string;
+
+  @Field(() => Company, { nullable: true })
+  company?: Company;
+
+  @Field(() => String, { nullable: true })
+  leadId?: string;
+
+  @Field(() => Lead, { nullable: true })
+  lead?: Lead;
+
+  @Field(() => String, { nullable: true })
+  dealId?: string;
+
+  @Field(() => Deal, { nullable: true })
+  deal?: Deal;
+
+  @Field(() => String, { nullable: true })
+  activityId?: string;
+
+  @Field(() => Activity, { nullable: true })
+  activity?: Activity;
+
+  // System fields
+  @Field(() => String)
+  createdById: string;
+
+  @Field(() => User)
+  createdBy: User;
+
+  @Field(() => Date)
+  createdAt: Date;
+
+  @Field(() => Date)
+  updatedAt: Date;
+
+  @Field(() => Date, { nullable: true })
+  deletedAt?: Date;
+}
